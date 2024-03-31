8bcfb5ad2c627fb566892719311b963ce2195b2f
Richest <rtran2477@conestogac.on.ca>
2024-03-31 15:56:34 -0400

    can track mouse y position and track on off of label

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 console.log('hello')
 const testEl =document.getElementById('test-el')
 testEl.textContent='bye bye'
+testEl.addEventListener('click',()=>{
+  console.log('clicked')
+  testEl.innerHTML = "<b> clicked </b>"
+})
+testEl.addEventListener('mouseover',()=>{
+  console.log('on')
+})
+testEl.addEventListener('mouseout',()=>{
+  console.log('off')
+})
+document.addEventListener('scroll',()=>{
+  const positionY=window.scrollY
+  console.log(positionY)
+})
