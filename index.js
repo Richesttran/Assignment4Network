2c4079d60de9549eafa2c54aceaf175db4714bb4
Richest <rtran2477@conestogac.on.ca>
2024-03-31 16:16:16 -0400

    can now make a request to an api in 3 different ways

--- index.js
+++ index.js
@@ -15,3 +15,27 @@ document.addEventListener('scroll',()=>{
   const positionY=window.scrollY
   console.log(positionY)
 })
+const url = 'https://swapi.dev/api/people/'
+$.ajax({
+  type:'GET',
+  url: url,
+  success: function(response){
+    console.log('jquery ajax',response)
+  },
+  error: function(error){
+    console.log(error)
+  }
+})
+const req = new XMLHttpRequest()
+
+req.addEventListener('readystatechange',()=>{
+  if(req.readyState===4){
+    console.log('xhttp',JSON.parse(req.responseText))
+  }
+})
+req.open('GET',url)
+req.send()
+
+fetch(url)
+.then(resp=>resp.json()).then(data=>console.log('fetch',data))
+.catch(err=> console.log(err))// catching error
