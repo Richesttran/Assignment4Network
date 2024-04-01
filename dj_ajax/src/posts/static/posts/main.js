97030a853804405a956b4ede3fe8623114ece582
Richest <rtran2477@conestogac.on.ca>
2024-04-01 11:58:33 -0400

    we can get the post data from the server and display it into the html

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,3 +1,31 @@
-console.log('hello world')
 const helloWorldBox=document.getElementById('hello-world')
-helloWorldBox.innerHTML="hello <b> world"
+const postBox=document.getElementById('posts-box')
+
+$.ajax({
+  type:'GET',
+  url:'/hello-world/',
+  success: function(response){
+    console.log('success',response.text)
+    helloWorldBox.textContent=response.text
+  },
+  error: function(error){
+    console.log('error',error)
+  }
+})
+$.ajax({
+  type:'GET',
+  url:'/data/',
+  success: function(response){
+    console.log(response)
+    const data= response.data
+    console.log(data)
+    data.forEach(el => {
+      postBox.innerHTML+=`
+      ${el.title}- <b>${el.body}</b><br>
+      `// ``allows for multi lines and inject data add
+    });
+  },
+  error: function(error){
+    console.log('error',error)
+  }
+})
