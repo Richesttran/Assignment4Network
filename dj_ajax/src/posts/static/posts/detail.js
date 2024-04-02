77b8daaa894143ccaf4fb508ba4ee1ba5a2e1470
Richest <rtran2477@conestogac.on.ca>
2024-04-02 10:35:33 -0400

    can now update posts and functional modal ui for delete and update button

--- dj_ajax/src/posts/static/posts/detail.js
+++ dj_ajax/src/posts/static/posts/detail.js
@@ -5,6 +5,9 @@ const updateBtn= document.getElementById('update-btn')
 const deleteBtn = document.getElementById('delete-btn')
 const url = window.location.href +"data/"
 const spinnerBox = document.getElementById('spinner-box')
+
+const titleInput= document.getElementById('id_title')
+const bodyInput= document.getElementById('id_body')
 backBtn.addEventListener('click',()=>{
   history.back()
 })
@@ -35,6 +38,8 @@ $.ajax({
     postBox.appendChild(titleEl)
     postBox.appendChild(bodyEl)
 
+    titleInput.value=data.title
+    bodyInput.value=data.body
     spinnerBox.classList.add('not-visible')
   },
   error:function(error){
