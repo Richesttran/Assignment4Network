567891a59d1ce8eef1a5ab1de10a3197a6ff3324
Richest <rtran2477@conestogac.on.ca>
2024-04-02 09:57:53 -0400

    fixed spinner and now can view post data in console

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -4,7 +4,7 @@ from .views import(
   load_post_data_view,
   like_unlike_post,
   post_detail,
-
+  post_detail_data_view
 )
 app_name='posts'
 urlpatterns=[
@@ -12,5 +12,5 @@ urlpatterns=[
   path('like-unlike/',like_unlike_post,name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
   path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
-
+path('<pk>/data/', post_detail_data_view, name='posts-detail-data'),
 ]
